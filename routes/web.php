636da954adbd86c180c14c8294efcb26f9eb4726
Yaznarb <leoselimlegrand@gmail.com>
2023-10-15 21:35:59 +0200

    added auth with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VolsController;
 use App\Http\Controllers\AeroportsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,10 +16,9 @@ use App\Http\Controllers\AeroportsController;
 |
 */
 
-
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+ Route::get('/welcome', function () {
+     return view('welcome');
+ });
 
 Route::get('/', [VolsController::class, 'show']);
 Route::get('/tableau', [VolsController::class, 'index']);
@@ -29,3 +30,14 @@ Route::get('/aeroport/{aeroports}/edit', [AeroportsController::class, 'edit'])->
 Route::put('/aeroport/{aeroports}/update', [AeroportsController::class, 'update'])->name('aeroports.update');
 Route::delete('/aeroport/{aeroports}/destroy', [AeroportsController::class, 'destroy'])->name('aeroports.destroy');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
