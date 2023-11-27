343d5411b27dc08138db071ca65c3724a5b91a42
Yaznarb <leoselimlegrand@gmail.com>
2023-11-27 12:03:20 +0100

    Send mail on account creation (to Mailtrap)

---
+++ resources/views/welcomeMail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Laravel Exemple Email</title>
+</head>
+<body>
+
+ <h1>Félicitation votre compte à bien été créé</h1>
+ {{-- <p>Merci, {{ $user->name }}</p> --}}
+
+</body>
+</html> 
